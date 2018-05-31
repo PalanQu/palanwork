c9d4c08756a36702642fa176ddbb500c2d1dec6b
qujiabao <palan_work@163.com>
2018-05-31 15:19:28 +0800

    update paras

--- algorithm/deeplearning/keras/emotion_classification.py
+++ algorithm/deeplearning/keras/emotion_classification.py
@@ -13,8 +13,8 @@ from nltk.corpus import stopwords
 from sklearn.utils import shuffle
 
 def to_vector(sentence_list):
-    MAX_TOKEN_NUM_WORDS = 3000
-    MAX_SEQUENCES_LEN = 50
+    MAX_TOKEN_NUM_WORDS = 2000
+    MAX_SEQUENCES_LEN = 1000
 
     tokenizer = Tokenizer(num_words=MAX_TOKEN_NUM_WORDS)
     tokenizer.fit_on_texts(sentence_list)
@@ -27,19 +27,20 @@ def to_vector(sentence_list):
 
 def clean_data(data_pandas):
     data_pandas.loc[data_pandas.label == 4, 'label'] = 1
-    data_pandas = shuffle(data_pandas)
     X = np.array(data_pandas['sentence'])
     y = np.array(data_pandas['label'])
     return X, y
 
 
-def read_data():
+def read_data(frac=1.0):
     FILE_PATH = 'data'
     FILE_NAME = 'emotion_data.csv'
 
     data_pandas = pd.read_csv(os.path.join(FILE_PATH, FILE_NAME), \
         names=['label','time_1','time_2','if_query','if_special','sentence'], \
         usecols=['label', 'sentence'])
+    data_pandas = shuffle(data_pandas)
+    data_pandas = data_pandas.sample(frac=frac)
     return data_pandas 
 
 def split_data(X, y, *, train_size=0.99, val_size=0.008):
@@ -50,9 +51,9 @@ def split_data(X, y, *, train_size=0.99, val_size=0.008):
 
 
 def build_model(x_train, y_train, x_test, y_test):
-    MAX_FEATURES = 2000 
+    MAX_FEATURES = 2000
     BATCH_SIZE = 32
-    EPOCHS = 5
+    EPOCHS = 16
     print('Build model...')
     model = Sequential()
     model.add(Embedding(MAX_FEATURES, 128))
@@ -77,29 +78,36 @@ def build_model(x_train, y_train, x_test, y_test):
 
 
 def predict(x):
-    model = load_model('model/classification_model')
+    model = load_model('model/test_model')
     y_hat = model.predict(x).reshape(x.shape[0])
     y_hat_binary = np.array(list(map(lambda x: 1 if (x > 0.5) else 0, y_hat)))
     return y_hat_binary
 
 def save_test_result(X_test, y_test, y_hat):
     test_data = pd.DataFrame({'x_test': X_test, 'y_test': y_test, 'y_hat': y_hat})
-    test_data.to_csv('test_result')
+    test_data.to_csv('test/test_result', index=False)
+
+def calculate_acc():
+    result_df = pd.read_csv('test/test_result')
+    result_df['if_equals'] = result_df.y_test == result_df.y_hat
+    equals_num = result_df[result_df.if_equals == True].count(numeric_only=True)['if_equals']
+    all_num = result_df.count(numeric_only=True)['if_equals']
+    return equals_num / all_num
 
 
 def main():
     # lable:1 = positive label:0 = negavite
-    data_df = read_data()
+    data_df = read_data(0.05)
     X, y = clean_data(data_df)
     X_train_text, X_val_text, X_test_text, y_train, y_val, y_test = split_data(X, y)
     X_train, X_val, X_test = to_vector(X_train_text), to_vector(X_val_text), to_vector(X_test_text)
-
-
     model = build_model(X_train, y_train, X_val, y_val)
-    model.save('text_model')
+    model.save('model/test_model')
 
     y_hat = predict(X_test)
-    save_test_result(X_test, y_test, y_hat)
+    save_test_result(X_test_text, y_test, y_hat)
+    rate = calculate_acc()
+    print(rate)
 
 
 if __name__ == '__main__':
